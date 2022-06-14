3e01a5ef32c076a92bdc9e1e70375d1a3e4e3a24
Lyn <lynfinman@gmail.com>
2022-06-13 21:28:52 -0400

    load questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+// App
+// - AdminNavBar
+// - QuestionForm
+// - QuestionList
+// ---QuestionItem
+
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm />
+      ) : (
+        <QuestionList questions={questions} />
+      )}
     </main>
   );
 }
